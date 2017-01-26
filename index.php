5fb97d97397d7ca1a91df78d8141c3b3a514db42
jonathanhamler <jkhamler@gmail.com>
2017-01-26 11:45:03 +0000

    Improvements

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
-  require_once('connection.php');
+require_once('connection.php');
 
-  if (isset($_GET['controller']) && isset($_GET['action'])) {
+if (isset($_GET['controller']) && isset($_GET['action'])) {
     $controller = $_GET['controller'];
-    $action     = $_GET['action'];
-  } else {
-    $controller = 'pages';
-    $action     = 'home';
-  }
+    $action = $_GET['action'];
+} else {
+    $controller = 'users';
+    $action = 'register';
+}
 
-
-  require_once('views/layout.php');
+require_once('views/layout.php');
 ?>
